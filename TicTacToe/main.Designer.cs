7bff14878d6539379628ac56b85f710c08bad19c
keyfkeyf <64215275+keyfkeyf@users.noreply.github.com>
2020-04-25 08:28:55 +0300

    Add files via upload

---
+++ TicTacToe/main.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace TicTacToe
+{
+    partial class main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameFieldPanel = new System.Windows.Forms.Panel();
+            this.field_22 = new System.Windows.Forms.Button();
+            this.field_12 = new System.Windows.Forms.Button();
+            this.field_02 = new System.Windows.Forms.Button();
+            this.field_21 = new System.Windows.Forms.Button();
+            this.field_11 = new System.Windows.Forms.Button();
+            this.field_01 = new System.Windows.Forms.Button();
+            this.field_20 = new System.Windows.Forms.Button();
+            this.field_10 = new System.Windows.Forms.Button();
+            this.field_00 = new System.Windows.Forms.Button();
+            this.b_StartVsPlayer = new System.Windows.Forms.Button();
+            this.b_StartVsCpu = new System.Windows.Forms.Button();
+            this.l_xResult = new System.Windows.Forms.Label();
+            this.l_oResult = new System.Windows.Forms.Label();
+            this.xResult = new System.Windows.Forms.Label();
+            this.oResult = new System.Windows.Forms.Label();
+            this.gameFieldPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameFieldPanel
+            // 
+            this.gameFieldPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gameFieldPanel.Controls.Add(this.field_22);
+            this.gameFieldPanel.Controls.Add(this.field_12);
+            this.gameFieldPanel.Controls.Add(this.field_02);
+            this.gameFieldPanel.Controls.Add(this.field_21);
+            this.gameFieldPanel.Controls.Add(this.field_11);
+            this.gameFieldPanel.Controls.Add(this.field_01);
+            this.gameFieldPanel.Controls.Add(this.field_20);
+            this.gameFieldPanel.Controls.Add(this.field_10);
+            this.gameFieldPanel.Controls.Add(this.field_00);
+            this.gameFieldPanel.Location = new System.Drawing.Point(18, 18);
+            this.gameFieldPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.gameFieldPanel.Name = "gameFieldPanel";
+            this.gameFieldPanel.Size = new System.Drawing.Size(299, 297);
+            this.gameFieldPanel.TabIndex = 0;
+            // 
+            // field_22
+            // 
+            this.field_22.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_22.Location = new System.Drawing.Point(201, 202);
+            this.field_22.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_22.Name = "field_22";
+            this.field_22.Size = new System.Drawing.Size(82, 85);
+            this.field_22.TabIndex = 17;
+            this.field_22.UseVisualStyleBackColor = true;
+            // 
+            // field_12
+            // 
+            this.field_12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_12.Location = new System.Drawing.Point(110, 202);
+            this.field_12.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_12.Name = "field_12";
+            this.field_12.Size = new System.Drawing.Size(82, 85);
+            this.field_12.TabIndex = 16;
+            this.field_12.UseVisualStyleBackColor = true;
+            // 
+            // field_02
+            // 
+            this.field_02.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_02.Location = new System.Drawing.Point(18, 202);
+            this.field_02.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_02.Name = "field_02";
+            this.field_02.Size = new System.Drawing.Size(82, 85);
+            this.field_02.TabIndex = 15;
+            this.field_02.UseVisualStyleBackColor = true;
+            // 
+            // field_21
+            // 
+            this.field_21.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_21.Location = new System.Drawing.Point(201, 108);
+            this.field_21.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_21.Name = "field_21";
+            this.field_21.Size = new System.Drawing.Size(82, 85);
+            this.field_21.TabIndex = 14;
+            this.field_21.UseVisualStyleBackColor = true;
+            // 
+            // field_11
+            // 
+            this.field_11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_11.Location = new System.Drawing.Point(110, 108);
+            this.field_11.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_11.Name = "field_11";
+            this.field_11.Size = new System.Drawing.Size(82, 85);
+            this.field_11.TabIndex = 13;
+            this.field_11.UseVisualStyleBackColor = true;
+            // 
+            // field_01
+            // 
+            this.field_01.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_01.Location = new System.Drawing.Point(18, 108);
+            this.field_01.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_01.Name = "field_01";
+            this.field_01.Size = new System.Drawing.Size(82, 85);
+            this.field_01.TabIndex = 12;
+            this.field_01.UseVisualStyleBackColor = true;
+            // 
+            // field_20
+            // 
+            this.field_20.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_20.Location = new System.Drawing.Point(201, 14);
+            this.field_20.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_20.Name = "field_20";
+            this.field_20.Size = new System.Drawing.Size(82, 85);
+            this.field_20.TabIndex = 11;
+            this.field_20.UseVisualStyleBackColor = true;
+            // 
+            // field_10
+            // 
+            this.field_10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_10.Location = new System.Drawing.Point(110, 14);
+            this.field_10.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_10.Name = "field_10";
+            this.field_10.Size = new System.Drawing.Size(82, 85);
+            this.field_10.TabIndex = 10;
+            this.field_10.UseVisualStyleBackColor = true;
+            // 
+            // field_00
+            // 
+            this.field_00.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.field_00.Location = new System.Drawing.Point(18, 14);
+            this.field_00.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.field_00.Name = "field_00";
+            this.field_00.Size = new System.Drawing.Size(82, 85);
+            this.field_00.TabIndex = 9;
+            this.field_00.UseVisualStyleBackColor = true;
+            // 
+            // b_StartVsPlayer
+            // 
+            this.b_StartVsPlayer.Location = new System.Drawing.Point(18, 325);
+            this.b_StartVsPlayer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.b_StartVsPlayer.Name = "b_StartVsPlayer";
+            this.b_StartVsPlayer.Size = new System.Drawing.Size(142, 83);
+            this.b_StartVsPlayer.TabIndex = 1;
+            this.b_StartVsPlayer.Text = "Начать игру против человека";
+            this.b_StartVsPlayer.UseVisualStyleBackColor = true;
+            this.b_StartVsPlayer.Click += new System.EventHandler(this.startVsPlayer_Click);
+            // 
+            // b_StartVsCpu
+            // 
+            this.b_StartVsCpu.Location = new System.Drawing.Point(175, 325);
+            this.b_StartVsCpu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.b_StartVsCpu.Name = "b_StartVsCpu";
+            this.b_StartVsCpu.Size = new System.Drawing.Size(142, 83);
+            this.b_StartVsCpu.TabIndex = 2;
+            this.b_StartVsCpu.Text = "Начать игру против Бота";
+            this.b_StartVsCpu.UseVisualStyleBackColor = true;
+            this.b_StartVsCpu.Click += new System.EventHandler(this.startVsCpu_Click);
+            // 
+            // l_xResult
+            // 
+            this.l_xResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.l_xResult.ForeColor = System.Drawing.Color.Blue;
+            this.l_xResult.Location = new System.Drawing.Point(31, 411);
+            this.l_xResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.l_xResult.Name = "l_xResult";
+            this.l_xResult.Size = new System.Drawing.Size(46, 71);
+            this.l_xResult.TabIndex = 3;
+            this.l_xResult.Text = "X:";
+            this.l_xResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // l_oResult
+            // 
+            this.l_oResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.l_oResult.ForeColor = System.Drawing.Color.Red;
+            this.l_oResult.Location = new System.Drawing.Point(204, 413);
+            this.l_oResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.l_oResult.Name = "l_oResult";
+            this.l_oResult.Size = new System.Drawing.Size(46, 71);
+            this.l_oResult.TabIndex = 4;
+            this.l_oResult.Text = "0:";
+            this.l_oResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // xResult
+            // 
+            this.xResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.xResult.ForeColor = System.Drawing.Color.Black;
+            this.xResult.Location = new System.Drawing.Point(66, 413);
+            this.xResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.xResult.Name = "xResult";
+            this.xResult.Size = new System.Drawing.Size(84, 71);
+            this.xResult.TabIndex = 5;
+            this.xResult.Text = "0";
+            this.xResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // oResult
+            // 
+            this.oResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.oResult.ForeColor = System.Drawing.Color.Black;
+            this.oResult.Location = new System.Drawing.Point(233, 413);
+            this.oResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.oResult.Name = "oResult";
+            this.oResult.Size = new System.Drawing.Size(84, 71);
+            this.oResult.TabIndex = 6;
+            this.oResult.Text = "0";
+            this.oResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 491);
+            this.Controls.Add(this.oResult);
+            this.Controls.Add(this.xResult);
+            this.Controls.Add(this.l_oResult);
+            this.Controls.Add(this.l_xResult);
+            this.Controls.Add(this.b_StartVsCpu);
+            this.Controls.Add(this.b_StartVsPlayer);
+            this.Controls.Add(this.gameFieldPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Крестики нолики";
+            this.gameFieldPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gameFieldPanel;
+        private System.Windows.Forms.Button field_22;
+        private System.Windows.Forms.Button field_12;
+        private System.Windows.Forms.Button field_02;
+        private System.Windows.Forms.Button field_21;
+        private System.Windows.Forms.Button field_11;
+        private System.Windows.Forms.Button field_01;
+        private System.Windows.Forms.Button field_20;
+        private System.Windows.Forms.Button field_10;
+        private System.Windows.Forms.Button field_00;
+        private System.Windows.Forms.Button b_StartVsPlayer;
+        private System.Windows.Forms.Button b_StartVsCpu;
+        private System.Windows.Forms.Label l_xResult;
+        private System.Windows.Forms.Label l_oResult;
+        private System.Windows.Forms.Label xResult;
+        private System.Windows.Forms.Label oResult;
+    }
+}
+
---
+++ TicTacToe/main.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class main : Form
+    {
+        private bool vsCpu;
+        // Флаг, указывающий на то, кому принадлежит следующий ход
+        private bool sym;
+        private bool fin;
+        private Random r;
+        private int xResultVal;
+        private int oResultVal;
+        public main()
+        {
+            InitializeComponent();
+            init();
+        }
+        // Инициализатор класса
+        private void init()
+        {
+            //Ставим результаты побед в 0
+            xResultVal = oResultVal = 0;
+            r = new Random();
+            fin = false;
+            //По умолчанию первым будет ходить 'крестик'
+            sym = false;
+            //По умолчанию, мы играем против человека
+            vsCpu = false;
+            initButtonsEvents();
+        }
+        private void initButtonsEvents()
+        {
+            foreach (Button but in gameFieldPanel.Controls)
+                but.Click += But_Click;
+        }
+        // Обработчик события клика по кнопке на игровом поле
+        private void But_Click(object sender, EventArgs e)
+        {
+            //Если игра всё ещё идёт
+            if (!fin)
+            {
+                Button but = (Button)sender;
+                //Проставляем на кнопке соответствующее значение
+                setButtonValue(but);
+                //Проверка завершения игры на этом ходе
+                checkFinal();
+                //Если у нас игра ещё не закончена, и идёт против ПК
+                if (!fin && vsCpu)
+                    //Делаем ход компьютера
+                    cpuWork();
+            }
+        }
+        // Клик по кнопке запуска игры против бота
+        private void startVsPlayer_Click(object sender, EventArgs e)
+        {
+            //Очищаем игровое поле
+            clearGameField();
+            //Указываем, против кого играем
+            vsCpu = false;
+            //Указываем, что игра началась
+            fin = false;
+            //По умолчанию первым будет ходить 'крестик'
+            sym = false;
+        }
+        private void startVsCpu_Click(object sender, EventArgs e)
+        {
+            int x;
+            //Очищаем игровое поле
+            clearGameField();
+            //Указываем, против кого играем
+            vsCpu = true;
+            //Указываем, что игра началась
+            fin = false;
+            //По умолчанию первым будет ходить 'крестик'
+            sym = false;
+        }
+        // Очищаем игровое поле
+        private void clearGameField()
+        {
+            //Проходимся по всем контроллам, на панели с кнопками
+            foreach (Button but in gameFieldPanel.Controls)
+            {
+                //Сбрасываем текст
+                but.Text = "";
+                //Сбрасываем цвет
+                but.ForeColor = Color.Black;
+            }
+        }
+        // Проставляем значение на кнопке
+        private void setButtonValue(Button but)
+        {
+            //Если на этой кнопке уже есть значение
+            if (but.Text.Length > 0)
+                //Выводим сообщение об этом
+                MessageBox.Show("Сюда поставить нельзя!");
+            //Если кнопка "пустая"
+            else
+            {
+                //Если "Крестик"
+                if (sym)
+                {
+                    //Ставим крестик на кнопке
+                    but.Text = "X";
+                    //Красим текст на ней в синий цвет (чисто для красоты)
+                    but.ForeColor = Color.Blue;
+                }
+                //Если "Нолик"
+                else
+                {
+                    //Ставим нолик на кнопке
+                    but.Text = "0";
+                    //Красим текст на ней в красный цвет (чисто для красоты)
+                    but.ForeColor = Color.Red;
+                }
+
+                //Меняем значение на противоположное
+                sym = !sym;
+            }
+        }
+        // Проверка завершения игры на этом ходе
+        private void checkFinal()
+        {
+            /*
+            Так вот, мы собираем все координаты выбранных ноликов и 
+            крестиков, и просто сравниваем их со списком уже известных решений.
+            */
+
+            //Инициализируем массив выигрышных комбинаций
+            string[][] winResults = new string[][]
+            {
+                new string[] {"00", "01", "02"},
+                new string[] {"10", "11", "12"},
+                new string[] {"20", "21", "22"},
+                new string[] {"00", "10", "20"},
+                new string[] {"01", "11", "21"},
+                new string[] {"02", "12", "22"},
+                new string[] {"00", "11", "22"},
+                new string[] {"02", "11", "20"}
+            };
+
+            //Результаты нажатий
+            List<string> xValues = new List<string>();
+            List<string> OValues = new List<string>();
+
+            //Проходимся по всем контроллам, на панели с кнопками
+            foreach (Button but in gameFieldPanel.Controls)
+            {
+                //Если на кнопке крестик
+                if (but.Text == "X")
+                    //Добавляем координаты из названия в список крестика
+                    xValues.Add(but.Name.Replace("field_", ""));
+                //Если на кнопке нолик
+                else if (but.Text == "0")
+                    //Добавляем координаты из названия в список нолика
+                    OValues.Add(but.Name.Replace("field_", ""));
+            }
+
+            //Если победили крестики
+            if(checkWin(winResults, xValues))
+            {
+                //Игра завершена
+                fin = true;
+                //Выводим сообщение о победе
+                MessageBox.Show("Победили крестики!");
+                //Увеличиваем количество побед крестиков
+                xResultVal++;
+                //Обновляем значения результатов побед
+                updateVinResults();
+            }
+            //Если победили нолики
+            else if(checkWin(winResults, OValues))
+            {
+                //Игра завершена
+                fin = true;
+                //Выводим сообщение о победе
+                MessageBox.Show("Победили нолики!");
+                //Увеличиваем количество побед ноликов
+                oResultVal++;
+                //Обновляем значения результатов побед
+                updateVinResults();
+            }
+            //Если наставлено уже 9 знаков
+            else if(OValues.Count + xValues.Count == 9)
+            {
+                //Игра завершена
+                fin = true;
+                //Выводим сообщение о победе
+                MessageBox.Show("Ничья!");
+            }
+            //В остальных случаях - игра идёт дальеше
+        }
+        // Обновляем значения результатов побед
+        private void updateVinResults()
+        {
+            //Обновляем значения результатов побед
+            xResult.Text = xResultVal.ToString();
+            oResult.Text = oResultVal.ToString();            
+        }
+        // Проверяем существующие комбинации с выигрышными
+        private bool checkWin(string[][] winResults, List<string> values)
+        {
+            bool ex = true;
+            
+            //Проходимся по списку выигрышных комбинаций
+            for(int i = 0; i < winResults.Length; i++)
+            {
+                //Ставим флаг в true
+                ex = true;
+                //Проходимся по всем цифрам комбинации
+                for (int j = 0; j < winResults[i].Length; j++)
+                {
+                    //Проверяем нахождение комбинации, 
+                    //при помощи логического сложения
+                    ex = ex && values.Contains(winResults[i][j]);
+                }
+                //Если хоть одной цифры из комбинации не найдётся среди 
+                //отмеченных, это будет считаться проигрышем
+
+                //Если все цифры комбинации мы нашли в списке
+                if (ex)
+                    //Выходим из массива
+                    break;
+            }
+
+            return ex;
+        }
+        // Ход "Компьютера"
+        private void cpuWork()
+        {
+            Button but;
+
+            do
+            {
+                //Получаем случайную кнопку, из списка тех, 
+                //что расположены на игровой панели
+                but = (Button)gameFieldPanel.Controls[r.Next(0, gameFieldPanel.Controls.Count)];
+
+                //Цикл идёт до тех пор, пока игра не закончится 
+                //(чисто на всякий случай проверка)
+                //Либо, не будет найдена случайная,
+                //свободная от значка кнопка
+            } while (!fin && (but.Text.Length > 0));
+
+            //Проставляем на кнопке соответствующее значение
+            setButtonValue(but);
+            //Проверка завершения игры на этом ходе
+            checkFinal();
+        }
+    }
+}
